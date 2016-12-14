68ca8335647221ec2c60ab800042cf2da6e08d60
Ankur Toshniwal <ankur@getfbpay.in>
2016-12-13 16:56:08 -0800

    switched to postgres database

--- config.py
+++ config.py
@@ -2,10 +2,12 @@ DEBUG = True
 
 # Set app directory
 import os
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
-# Production DB
+# BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+
+# Production DBS
 # SQLALCHEMY_DATABASE_URI = 'mysql://hhscusername:freepassword@hhscdb.ci7xsupmx76b.us-east-1.rds.amazonaws.com/hhscdb'
+SQLALCHEMY_DATABASE_URI = 'postgres://otxfbmyruvjrqp:be285e1a17020371dc63cfd88debf4a52f46129506422333fa54ce7ce32be994@ec2-54-225-118-55.compute-1.amazonaws.com:5432/d4lolf30b4fm4m'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(BASE_DIR, 'db_repository')
 
 # print __file__
@@ -15,8 +17,8 @@ THREADS_PER_PAGE = 5
 # email server
 # MAIL_SERVER = 'smtp.googlemail.com'
 # MAIL_PORT = 587
-MAIL_USERNAME = 'ankurtoshniwal4@gmail.com'
-MAIL_PASSWORD = 'R#6wny9gat'
+MAIL_USERNAME = 'hhsc.register@gmail.com'
+MAIL_PASSWORD = 'weeks2+2'
 MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 587
 MAIL_USE_TLS = True
